aa14c5f7bc7e3d691d39112232079bfa86009f9f
netbuffer <javawiki@163.com>
2022-12-03 22:18:49 +0800

    test isAccess

--- src/main/java/cn/netbuffer/liteflow/demo/component/flow/Step1Component.java
+++ src/main/java/cn/netbuffer/liteflow/demo/component/flow/Step1Component.java
@@ -4,10 +4,18 @@ import com.alibaba.fastjson.JSONObject;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
 
 @Slf4j
 @LiteflowComponent(name = "step1")
 public class Step1Component extends NodeComponent {
+
+    @Override
+    public boolean isAccess() {
+        Boolean isAccess= RandomUtils.nextBoolean();
+        log.debug("step1 isAccess return {}",isAccess);
+        return isAccess;
+    }
 
     @Override
     public void process() throws Exception {
