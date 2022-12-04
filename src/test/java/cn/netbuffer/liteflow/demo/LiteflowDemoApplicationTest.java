587ec0d67dfb35042784b51eb263b4b5dcc0f7ec
netbuffer <javawiki@163.com>
2022-12-04 21:44:01 +0800

    validate EL

--- src/test/java/cn/netbuffer/liteflow/demo/LiteflowDemoApplicationTest.java
+++ src/test/java/cn/netbuffer/liteflow/demo/LiteflowDemoApplicationTest.java
@@ -1,5 +1,6 @@
 package cn.netbuffer.liteflow.demo;
 
+import com.yomahub.liteflow.builder.el.LiteFlowChainELBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,4 +13,12 @@ public class LiteflowDemoApplicationTest {
     public void init() {
         log.info("init");
     }
+
+    @Test
+    public void testEL() {
+        String el = "SWITCH(paySwitchStep).to(paySuccessStep, payFailStep)";
+        boolean isValid = LiteFlowChainELBuilder.validate(el);
+        log.debug("test el[{}] isValid={}", el, isValid);
+    }
+
 }
