dab9d1a89832ea82b99ab3cf92c34d070b182bc3
netbuffer <javawiki@163.com>
2022-12-11 20:16:43 +0800

    test getCmpData

--- src/main/java/cn/netbuffer/liteflow/demo/component/flow/normal/bean/methodlevel/LiteflowComponentsConfig.java
+++ src/main/java/cn/netbuffer/liteflow/demo/component/flow/normal/bean/methodlevel/LiteflowComponentsConfig.java
@@ -1,5 +1,6 @@
 package cn.netbuffer.liteflow.demo.component.flow.normal.bean.methodlevel;
 
+import com.alibaba.fastjson.JSONObject;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.annotation.LiteflowMethod;
 import com.yomahub.liteflow.core.NodeComponent;
@@ -14,6 +15,8 @@ public class LiteflowComponentsConfig {
     @LiteflowMethod(value = LiteFlowMethodEnum.PROCESS, nodeId = "hello", nodeType = NodeTypeEnum.COMMON)
     public void processHello(NodeComponent bindCmp) {
         log.debug("process hello component based method");
+        JSONObject data = bindCmp.getCmpData(JSONObject.class);
+        log.debug("get hello component config data={} key[name]={}", data, data.getString("name"));
     }
 
 }
