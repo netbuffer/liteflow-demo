9c912eec99f403b772d756c6018358fed12f768a
netbuffer <javawiki@163.com>
2022-12-30 14:59:58 +0800

    test exec long task

--- src/main/java/cn/netbuffer/liteflow/demo/component/flow/normal/persistence/RecordToElasticsearchComponent.java
+++ src/main/java/cn/netbuffer/liteflow/demo/component/flow/normal/persistence/RecordToElasticsearchComponent.java
@@ -3,6 +3,8 @@ package cn.netbuffer.liteflow.demo.component.flow.normal.persistence;
 import com.yomahub.liteflow.annotation.LiteflowComponent;
 import com.yomahub.liteflow.core.NodeComponent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.RandomUtils;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @LiteflowComponent(name = "recordToElasticsearchComponent")
@@ -10,7 +12,10 @@ public class RecordToElasticsearchComponent extends NodeComponent {
 
     @Override
     public void process() throws Exception {
-        log.debug("process");
+        log.debug("start exec process for elasticsearch");
+        log.debug("write to elasticsearch");
+        TimeUnit.SECONDS.sleep(RandomUtils.nextInt(3, 10));
+        log.debug("end exec process for elasticsearch");
     }
 
 }
