154a2bfd816b9753c0ce4d7fcb46523b9aa05661
netbuffer <javawiki@163.com>
2023-01-15 21:39:54 +0800

    test junit5 @BeforeEach/@BeforeAll/@AfterEach/@AfterAll/@Disabled

--- src/test/java/cn/netbuffer/liteflow/demo/Junit5Test.java
+++ src/test/java/cn/netbuffer/liteflow/demo/Junit5Test.java
@@ -1,8 +1,8 @@
 package cn.netbuffer.liteflow.demo;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @DisplayName("Junit5框架测试")
@@ -12,6 +12,44 @@ public class Junit5Test {
     @Test
     public void testDisplayName() {
         log.debug("@DisplayName");
+    }
+
+    @BeforeEach
+    public void testBeforeEach() {
+        log.debug("BeforeEach");
+    }
+
+    @BeforeAll
+    public static void testBeforeAll() {
+        log.debug("BeforeAll");
+    }
+
+    @AfterEach
+    public void testAfterEach() {
+        log.debug("AfterEach");
+    }
+
+    @AfterAll
+    public static void testAfterAll() {
+        log.debug("AfterAll");
+    }
+
+    @Test
+    @Tag("someTag")
+    public void testTag() {
+        log.debug("test Tag");
+    }
+
+    @Test
+    @Disabled
+    @Timeout(value = 3, unit = TimeUnit.SECONDS)
+    public void testTimeout() {
+        log.debug("test Timeout");
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
