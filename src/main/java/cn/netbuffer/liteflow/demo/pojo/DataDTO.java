22a6b25cd703633dd056a4d926a182104a40fd1b
netbuffer <javawiki@163.com>
2023-08-01 15:13:20 +0800

    add Lombok @Accessors

--- src/main/java/cn/netbuffer/liteflow/demo/pojo/DataDTO.java
+++ src/main/java/cn/netbuffer/liteflow/demo/pojo/DataDTO.java
@@ -1,8 +1,10 @@
 package cn.netbuffer.liteflow.demo.pojo;
 
 import lombok.Data;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain = true)
 public class DataDTO {
     public String payload;
     public String type;
